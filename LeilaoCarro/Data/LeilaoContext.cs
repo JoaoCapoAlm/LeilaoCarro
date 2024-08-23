0ee1e7921925e80c9b4bca7a48d72996476aadfe
João Pedro <jpcapoani98@gmail.com>
2024-08-23 11:57:20 -0300

    Feat:Config banco de dados

---
+++ LeilaoCarro/Data/LeilaoContext.cs
@@ -0,0 +1,21 @@
+﻿using LeilaoCarro.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeilaoCarro.Data
+{
+    public class LeilaoContext(DbContextOptions<LeilaoContext> options) : DbContext(options)
+    {
+        public DbSet<Usuario> Usuario { get; set; }
+        //public DbSet<UsuarioEndereco> UsuarioEndereco { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<UsuarioEndereco>()
+            //    .HasOne(x => x.Usuario)
+            //    .WithMany(x => x.UsuarioEnderecos)
+            //    .HasForeignKey(x => x.IdUsuario);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
