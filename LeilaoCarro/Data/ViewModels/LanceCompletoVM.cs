400d415f193438751730d50d55496b94e79bbfac
João Pedro <jpcapoani98@gmail.com>
2024-08-26 15:37:54 -0300

    merge

---
+++ LeilaoCarro/Data/ViewModels/LanceCompletoVM.cs
@@ -0,0 +1,26 @@
+﻿using LeilaoCarro.Data.ViewModels;
+
+namespace LeilaoCarro.Models
+{
+    public record LanceCompletoVM(
+        int Id,
+        UsuarioVM Usuario,
+        CarroVM Carro,
+        decimal Valor,
+        DateTime DataHoraLance
+    );
+
+    public static class LanceCompletoExtensions
+    {
+        public static LanceCompletoVM LanceCompletoToVM(this Lance lance)
+        {
+            return new LanceCompletoVM(
+                lance.Id,
+                lance.Usuario.UsuarioToVM(),
+                lance.Carro.CarroToVM(),
+                lance.Valor,
+                lance.DataHoraLance
+            );
+        }
+    }
+}
